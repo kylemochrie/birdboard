dbe2b5a6296ad3a8cf20c42190f52feb70082a65
AzureAD\KyleMochrie <kmochrie@solutechnology.com>
2019-08-01 16:18:55 -0400

    Added more test, added Tailwind, updated UI.

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,18 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>BirdBoard</title>
-</head>
-<body>
-  <h1>BirdBoard</h1>
-  <ul>
-    @foreach ($projects as $project) 
-      <li>{{ $project->title }}</li>
-      <li>{{ $project->description }}</li>
-    @endforeach
-  </ul>
-</body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+
+  <div class="flex items-center mb-3">
+    <a href="/projects/create">New Project</a>
+  </div>
+
+  <div class="flex">
+    @forelse ($projects as $project) 
+    <div class="bg-white mr-4 p-5 rounded shadow w-1/3" style="height: 200px">
+      <h3 class="font-normal text-xl py-4">{{ $project->title }}</h3>
+      <div class="text-gray-400">{{ str_limit($project->description, 100) }}</div>
+    </div>
+    @empty
+      <div>No projects yet.</div>
+    @endforelse
+  </div>
+
+@endsection
