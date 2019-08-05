b4965ca226aa272d22cf8e7a06de49ab80eac1dd
AzureAD\KyleMochrie <kmochrie@solutechnology.com>
2019-08-05 15:15:45 -0400

    Added tasks and updated UI

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -2,19 +2,21 @@
 
 @section('content')
 
-  <div class="flex items-center mb-3">
-    <a href="/projects/create">New Project</a>
-  </div>
-
-  <div class="flex">
-    @forelse ($projects as $project) 
-    <div class="bg-white mr-4 p-5 rounded shadow w-1/3" style="height: 200px">
-      <h3 class="font-normal text-xl py-4">{{ $project->title }}</h3>
-      <div class="text-gray-400">{{ str_limit($project->description, 100) }}</div>
+  <header class="flex items-center mb-3 py-3">
+    <div class="flex justify-between items-end w-full text-gray-400">
+      <h2>My Projects</h2>
+      <a href="/projects/create" class="button">New Project</a>
     </div>
+  </header>
+
+  <main class="lg:flex lg:flex-wrap -mx-3">
+    @forelse ($projects as $project) 
+      <div class="lg:w-1/3 px-3 pb-6">
+        @include('projects.card')
+      </div>
     @empty
       <div>No projects yet.</div>
     @endforelse
-  </div>
+  </main>
 
 @endsection
